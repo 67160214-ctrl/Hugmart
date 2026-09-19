01e5c9108ebe768e25eb453ba2584c532b1a64cf
67160214-ctrl <67160214@go.buu.ac.th>
2026-09-19 15:12:18 +0700

    Add files via upload

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-$host = 'localhost';
+$host = 'db';
 $dbname = 's67160214';   // ชื่อฐานข้อมูล (หากไม่ได้ผลให้ลองเปลี่ยนเป็น '67160214')
 $username = 's67160214'; // Username ตามภาพ
 $password = 'W5xPdkn9';  // Password ตามภาพ
